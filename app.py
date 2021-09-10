c80c54df705a4c6bbe0214815971be582ee1fece
hiraikiichi <m1m21329@st.oit.ac.jp>
2021-09-10 22:55:13 +0900

    ver check

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from flask import Flask
 
@@ -6,8 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    ver = sys.version
     # target = os.environ.get('TARGET', 'World')
-    return 'Hello!\n'
+    return 'Hello!{}\n'.format(ver)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8888)))
