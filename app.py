491a548a16dbaa0a060d3017af6cbc3e1c98a9f8
nrkt <nu.noken2018@gmail.com>
2021-09-14 10:59:20 +0900

    ADD: spotify api

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import sys
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 from flask import Flask, render_template, request
 
@@ -12,6 +14,10 @@ app = Flask(__name__)
 
 TOKEN = os.environ.get("TEST")
 # TOKEN = 334
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+CLIENT_CREDENTIALS_MANAGER = spotipy.oauth2.SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET)
+spotify = spotipy.Spotify(client_credentials_manager=CLIENT_CREDENTIALS_MANAGER)
 
 @app.route('/')
 def hello_world():
@@ -28,6 +34,17 @@ def show():
 @app.route('/about')
 def aboutPage():
     return render_template('about.html')
+
+def get_songs_from_playlist(playlist_id: str):
+    songs = []
+    result = spotify.playlist(playlist_id)
+    for item in result['tracks']['items']:
+        song_name = item['track']['name']
+        artist = item['track']['artists'][0]['name']
+        ref = item['track']['href']
+        # preview_url = item['track']['preview_url']
+        songs.append([song_name, artist, ref])
+    return songs
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8888)))
